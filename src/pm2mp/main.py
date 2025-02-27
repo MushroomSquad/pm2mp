ba18ae2f88c5576e9e7eb37c87bba0cab2eac8f2
Dmitriy Osipenko <osipenko.dmitriy@tseh85.ru>
2025-02-27 12:25:51 +0300

    typind and add update function

--- src/pm2mp/main.py
+++ src/pm2mp/main.py
@@ -1,3 +1,4 @@
+from asyncio.subprocess import Process
 import json
 from asyncio import Semaphore
 import asyncio
@@ -5,31 +6,35 @@ from subprocess import PIPE, Popen
 import pathlib
 import os
 import platform
-from typing import List
+from typing import Any, Coroutine, List
 
 from .models import PM2AppConfig, PM2Response
 from .decorator import dual_method
 
 
 class PM2MP:
-    def __init__(self, semaphore_value: int = 2, async_mode: bool = False) -> None:
-        self.node = "node"
-        self.wrapper_file = (
+    def __init__(
+        self,
+        semaphore_value: int = 2,
+        async_mode: bool = False,
+    ) -> None:
+        self.node: str = "node"
+        self.wrapper_file: str = (
             str(pathlib.Path(__file__).with_name("pm2wrapper.js")).replace(os.sep, "/")
             if platform.system() == "Windows"
             else str(pathlib.Path(__file__).with_name("pm2wrapper.js"))
         )
-        cpu_count = os.cpu_count()
+        cpu_count: int | None = os.cpu_count()
         self.semaphore: Semaphore = Semaphore(
             cpu_count if cpu_count else semaphore_value
         )
-        self.async_mode = async_mode
+        self.async_mode: bool = async_mode
 
     def _prepare_command(
         self,
-        command,
-        args,
-    ):
+        command: str,
+        args: dict,
+    ) -> List[str]:
         return [
             self.node,
             "-e",
@@ -40,36 +45,40 @@ class PM2MP:
 
     def _run_process(
         self,
-        program,
-    ):
-        process = Popen(
+        program: List[str],
+    ) -> str:
+        process: Popen = Popen(
             program,
             stdout=PIPE,
             stderr=PIPE,
             text=True,
         )
+        stdout: str
+        stderr: str
         stdout, stderr = process.communicate()
         return stdout if process.returncode == 0 else stderr
 
     async def _run_process_async(
         self,
-        program,
-    ):
+        program: List[str],
+    ) -> bytes:
         async with self.semaphore:
-            process = await asyncio.subprocess.create_subprocess_exec(
+            process: Process = await asyncio.subprocess.create_subprocess_exec(
                 *program,
                 stdout=PIPE,
                 stderr=PIPE,
             )
+            stdout: bytes
+            stderr: bytes
             stdout, stderr = await process.communicate()
             return stdout if process.returncode == 0 else stderr
 
     def _execude(
         self,
-        command,
+        command: str,
         args_list: List[PM2AppConfig],
-    ):
-        programs = [
+    ) -> List[PM2Response]:
+        programs: List[List[str]] = [
             self._prepare_command(command, args.model_dump_json(exclude_none=True))
             for args in args_list
         ]
@@ -85,14 +94,14 @@ class PM2MP:
 
     async def _execude_async(
         self,
-        command,
+        command: str,
         args_list: List[PM2AppConfig],
-    ):
-        programs = [
+    ) -> List[PM2Response]:
+        programs: List[List[str]] = [
             self._prepare_command(command, args.model_dump_json(exclude_none=True))
             for args in args_list
         ]
-        tasks = [
+        tasks: List[Coroutine[Any, Any, bytes]] = [
             self._run_process_async(
                 program,
             )
@@ -109,110 +118,124 @@ class PM2MP:
     def list(
         self,
         args: List[PM2AppConfig] = [],
-    ):
+    ) -> List[PM2Response]:
         return self._execude("list", args)
 
     @list.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("list", args)
 
     @dual_method
     def start(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("start", args)
 
     @start.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("start", args)
 
     @dual_method
     def stop(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("stop", args)
 
     @stop.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("stop", args)
 
     @dual_method
     def restart(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("restart", args)
 
     @restart.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("restart", args)
 
     @dual_method
     def reload(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("reload", args)
 
     @reload.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("reload", args)
 
     @dual_method
     def delete(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("delete", args)
 
     @delete.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("delete", args)
 
     @dual_method
     def kill(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("delete", args)
 
     @kill.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("delete", args)
 
     @dual_method
     def describe(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return self._execude("describe", args)
 
     @describe.register()
     async def _(
         self,
-        args: List = [PM2AppConfig],
-    ):
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
         return await self._execude_async("describe", args)
+
+    @dual_method
+    def update(
+        self,
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
+        return self._execude("update", args)
+
+    @update.register()
+    async def _(
+        self,
+        args: List[PM2AppConfig],
+    ) -> List[PM2Response]:
+        return await self._execude_async("update", args)
